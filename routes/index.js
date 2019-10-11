6f580f2ad379be4738b77e4c3af1d4766a6d4da7
Bryan Van Horn <bvhwa@yahoo.com>
2019-10-11 02:30:07 -0700

    Index file updates to show viewer count and link to streamer page

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,8 @@ router.post('/', function(req, res, next) {
   const name = req.body.name;
   const url = "https://www.twitch.tv/" + name;
 
+  // res.render('index', { 'online': online, 'offline': offline, 'loading': true });
+
   puppeteer
     .launch()
     .then(browser => {
@@ -30,25 +32,41 @@ router.post('/', function(req, res, next) {
       const $ = cheerio.load(html);
 
       let isLive = false;
+      let imageUrl, viewers;
 
       if($('div.live-indicator.tw-mg-l-05.tw-sm-mg-l-1.tw-visible').length){
         isLive = true;
       }
+      if($('img.tw-block.tw-border-radius-rounded.tw-image.tw-image-avatar').length){
+        // console.log($('img.tw-block.tw-border-radius-rounded.tw-image.tw-image-avatar').attr('src'));
+        imageUrl = $('img.tw-image-avatar').attr('src');
+      }
+      if($('div.tw-mg-l-05.tw-stat__value').length){
+        // console.log($('div.tw-mg-l-05.tw-stat__value').html());
+        viewers = $('div.tw-mg-l-05.tw-stat__value').html();
+      }
+
+      let streamer = {
+        viewers: viewers,
+        isLive: isLive,
+        imageUrl: imageUrl,
+        name: name,
+        twitchUrl: url
+      };
 
       if(isLive){
-        online.push(name);
+        online.push(streamer);
       }
       else{
-        offline.push(name);
+        offline.push(streamer);
       }
 
-      res.render('index', { 'online': online, 'offline': offline });
+      res.render('index', { 'online': online, 'offline': offline, 'loading': false });
     })
     .catch(error => {
       console.log(error);
-      res.render('index', { 'online': online, 'offline': offline });
+      res.render('index', { 'online': online, 'offline': offline, 'loading': false });
     });
-
 });
 
 module.exports = router;
